da5789ce88fbd3d2a6e69671170611064051ff23
abhijeetpalanki <ajpalanki@gmail.com>
2023-01-13 17:52:35 -0600

    Video detail component completed

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -5,7 +5,6 @@ import { CheckCircle } from "@mui/icons-material";
 
 import {
   demoThumbnailUrl,
-  demoVideoUrl,
   demoVideoTitle,
   demoChannelUrl,
   demoChannelTitle,
@@ -20,20 +19,20 @@ const VideoCard = ({
   return (
     <Card
       sx={{
-        width: { md: "320px", xs: "100%" },
+        width: { xs: "100%", sm: "358px", md: "320px" },
         boxShadow: "none",
         borderRadius: 0,
       }}
     >
-      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+      <Link to={`/video/${videoId}`}>
         <CardMedia
           image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
           alt={snippet?.title}
-          sx={{ width: 358, height: 180 }}
+          sx={{ width: { xs: "100%", sm: "358px", md: "320px" }, height: 180 }}
         />
       </Link>
       <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
-        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <Link to={`/video/${videoId}`}>
           <Typography variant="subtitle1" fontWeight="bold" color="#fff">
             {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
           </Typography>
