a68539182ca4ff97560ab03f1b3351eaf0000c60
abhijeetpalanki <ajpalanki@gmail.com>
2023-01-13 17:17:17 -0600

    functionality - search, channel and videos

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,6 +16,7 @@ export const logo = "/logo.jpg";
 
 export const categories = [
   { name: "New", icon: <HomeIcon /> },
+  { name: "JS Mastery", icon: <HomeIcon /> },
   { name: "Coding", icon: <CodeIcon /> },
   { name: "ReactJS", icon: <CodeIcon /> },
   { name: "NextJS", icon: <CodeIcon /> },
